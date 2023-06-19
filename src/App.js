9d135213d0716f5dc91ec32411ada24c93dacb86
SangNef <sang.tv2614@gmail.com>
2023-06-19 16:04:05 +0700

    setup Tailwwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [first, setFirst] = useState("Sang");
+  
   return (
     <>
-      
+      <h1>hello</h1>
     </>
   );
 }
